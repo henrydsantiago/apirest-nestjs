7f3d738c3bbc444783a698c73e5947e289df09bd
HENRY ALEXANDER D'SANTIAGO DELGADO <henrynator@MacBook-Air-de-HENRY.local>
2023-08-17 12:53:53 -0400

    Configurando variables de entorno con Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from "@nestjs/config";
+
 
 @Module({
-  imports: [ProductModule,  MongooseModule.forRoot('mongodb://localhost/db_products')],  
+  imports: [
+    ConfigModule.forRoot(),
+    ProductModule,  
+    MongooseModule.forRoot(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGOHOST}:${process.env.MONGOPORT}`)],  
   controllers: [AppController],
   providers: [AppService],
 })
